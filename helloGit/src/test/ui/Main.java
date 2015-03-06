39460936354d52c3f2174cd71d21d0302c1d887f
Leonie <quitschiii2305@googlemail.com>
2015-03-06 09:18:58 +0100

    Added new Branch and info to Main.java

--- helloGit/src/test/ui/Main.java
+++ helloGit/src/test/ui/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Dies ist ein test");
 		System.out.println("Test No #2");
+		System.out.println("Dies sollte nicht im Master-branch stehen!!");
 	}
 
 }
