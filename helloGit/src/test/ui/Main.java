f6e0f8ff0a033fd72b38e2a648a2bf12d3433d8d
Leonie <quitschiii2305@googlemail.com>
2015-03-06 10:44:30 +0100

    Update von Main.java

--- helloGit/src/test/ui/Main.java
+++ helloGit/src/test/ui/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Dies ist ein test");
 		System.out.println("Test No #2");
-		System.out.println("Dies sollte nicht im Master-branch stehen!!");
+		System.out.println("Dies sollte auch im Master-branch stehen!!");
+		System.out.println("UPDATE");
 	}
 
 }
